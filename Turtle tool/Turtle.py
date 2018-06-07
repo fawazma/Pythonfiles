bc1fd8bb593806f5c9ae3f35249b6c0cb3c9e7fb
Fawaz Bukhowa <39864826+fawazma@users.noreply.github.com>
2018-06-07 04:58:15 -0700

    Add files via upload

---
+++ Turtle tool/Turtle.py
@@ -0,0 +1,20 @@
+import turtle
+
+turtle.backward(90)
+turtle.left(140)
+turtle.backward(90)
+turtle.left(140)
+turtle.backward(90)
+turtle.left(140)
+turtle.backward(90)
+turtle.left(140)
+turtle.backward(90)
+turtle.left(140)
+turtle.backward(90)
+turtle.left(140)
+turtle.backward(90)
+turtle.left(140)
+
+
+
+
