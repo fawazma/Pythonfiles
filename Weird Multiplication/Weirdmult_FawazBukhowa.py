97da86a0542ef18f7ff8f81886816a95df262e9c
Fawaz Bukhowa <39864826+fawazma@users.noreply.github.com>
2018-06-08 19:51:37 -0700

    Add files via upload

---
+++ Weird Multiplication/Weirdmult_FawazBukhowa.py
@@ -0,0 +1,207 @@
+#Programming Project, Weird multiplication.
+import math
+
+A = 32
+B = 23
+
+#a
+
+print("A:", A*2,"B:" ,B//2)                 #Add A to the product, B is odd.
+print("A:", A*2*2,"B:" ,B//2//2)            #Add A to the product, B is odd.
+print("A:", A*2*2*2,"B:" ,B//2//2//2)       #Ignore this A, B is even.
+print("A:", A*2*2*2*2,"B:" ,B//2//2//2//2)  #Add A to the product, B is odd.
+
+#b
+
+product_str = input("Choose any product from A: ")
+product = int(product_str)
+
+while 0 <= product <= 512:
+    if product == 512:
+        print("Product is right!")
+        break
+    elif product == 128:
+        print("Product is right!")
+        break
+    elif product == 64:
+        print("product is right!")
+        break
+    else:
+        print("Sorry my friend, wrong product")
+        break
+
+product_str = input("Choose any product from A: ")
+product = int(product_str)
+
+while 0 <= product <= 512:
+    if product == 512:
+        print("Product is right!")
+        break
+    elif product == 128:
+        print("Product is right!")
+        break
+    elif product == 64:
+        print("product is right!")
+        break
+    else:
+        print("Sorry my friend, wrong product")
+        break
+
+product_str = input("Choose any product from A: ")
+product = int(product_str)
+
+while 0 <= product <= 512:
+    if product == 512:
+        print("Product is right!")
+        break
+    elif product == 128:
+        print("Product is right!")
+        break
+    elif product == 64:
+        print("product is right!")
+        break
+    else:
+        print("Sorry my friend, wrong product")
+        break
+
+product_str = input("Choose any product from A: ")
+product = int(product_str)
+
+while 0 <= product <= 512:
+    if product == 512:
+        print("Product is right!")
+        break
+    elif product == 128:
+        print("Product is right!")
+        break
+    elif product == 64:
+        print("product is right!")
+        break
+    else:
+        print("Sorry my friend, wrong product")
+        break
+
+
+product_str = input("Choose any product from A: ")
+product = int(product_str)
+
+while 0 <= product <= 512:
+    if product == 512:
+        print("Product is right!")
+        break
+    elif product == 128:
+        print("Product is right!")
+        break
+    elif product == 64:
+        print("product is right!")
+        break
+    else:
+        print("Sorry my friend, wrong product")
+        break
+
+
+product_str = input("Choose any product from A: ")
+product = int(product_str)
+
+while 0 <= product <= 512:
+    if product == 512:
+        print("Product is right!")
+        break
+    elif product == 128:
+        print("Product is right!")
+        break
+    elif product == 64:
+        print("product is right!")
+        break
+    else:
+        print("Sorry my friend, wrong product")
+        break
+
+product_str = input("Choose any product from A: ")
+product = int(product_str)
+
+while 0 <= product <= 512:
+    if product == 512:
+        print("Product is right!")
+        break
+    elif product == 128:
+        print("Product is right!")
+        break
+    elif product == 64:
+        print("product is right!")
+        break
+    else:
+        print("Sorry my friend, wrong product")
+        break
+
+
+
+product_str = input("Choose any product from A: ")
+product = int(product_str)
+
+while 0 <= product <= 512:
+    if product == 512:
+        print("Product is right!")
+        break
+    elif product == 128:
+        print("Product is right!")
+        break
+    elif product == 64:
+        print("product is right!")
+        break
+    else:
+        print("Sorry my friend, wrong product")
+        break
+
+product_str = input("Choose any product from A: ")
+product = int(product_str)
+
+while 0 <= product <= 512:
+    if product == 512:
+        print("Product is right!")
+        break
+    elif product == 128:
+        print("Product is right!")
+        break
+    elif product == 64:
+        print("product is right!")
+        break
+    else:
+        print("Sorry my friend, wrong product")
+        break
+
+product_str = input("Choose any product from A: ")
+product = int(product_str)
+
+while 0 <= product <= 512:
+    if product == 512:
+        print("Product is right!")
+        break
+    elif product == 128:
+        print("Product is right!")
+        break
+    elif product == 64:
+        print("product is right!")
+        break
+    else:
+        print("Sorry my friend, wrong product")
+        break
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
